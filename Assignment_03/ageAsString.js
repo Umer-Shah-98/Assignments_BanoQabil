774731c82adfa4cd2e8c7dd0ee94a8e45bd06e68
Umer Shah <66215639+Umer-Shah-98@users.noreply.github.com>
2023-03-28 01:49:44 +0500

    Add files via upload
    1- Declare a variable called “age” and assign it a value of 25. Convert the value of age to a string data type and store it in a new variable called “ageAsString”.
    2- Create a program that prompts the user to enter a number and then checks whether that number is even or odd. Display the result to the user using an alert box.
    3- Declare a variable called “temperature” and assign it a value of 72. Convert the temperature to Celsius and store the result in a new variable called “celsius”. Display the value of celsius to the console.
    4- Create a program that asks the user to enter their name and age. If the user’s age is greater than or equal to 18, display a message to the console that says “Welcome [name], you are an adult”. If the user’s age is less than 18, display a message that says “Sorry [name], you are not yet an adult”.
    5- Declare a variable called “isRaining” and assign it a value of true. Using an if...else statement, display a message to the console that says “Bring an umbrella” if it is raining, and “Leave the umbrella at home” if it is not raining.

---
+++ Assignment_03/ageAsString.js
@@ -0,0 +1,5 @@
+// Declare a variable called “age” and assign it a value of 25. Convert the value of age to a string data type and store it in a new variable called “ageAsString”.
+
+let age = 25;
+let ageAsString = age.toString();
+console.log(ageAsString);
---
+++ Assignment_03/temperature.js
@@ -0,0 +1,5 @@
+// Declare a variable called “temperature” and assign it a value of 72. Convert the temperature to Celsius and store the result in a new variable called “celsius”. Display the value of celsius to the console.
+      
+      let temperature = 72;
+      let celsius = ((temperature - 32) * 5) / 9;
+      console.log("The temperature is: " + celsius + "°C");
